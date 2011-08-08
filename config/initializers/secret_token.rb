e55ea36a39dbc0c91a616f06a6df01bae32236a8
vesco <vesco.miloushev@gmail.com>
2011-08-08 11:16:55 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Frozentap::Application.config.secret_token = '0eb55e00589bef7402c038847d83ac98f16af3beebe7be4fb939551cc9657019627f934596ca543d9386c7857f6a7824cf0fe5f82c839bc3a6b303214df8d1c8'
